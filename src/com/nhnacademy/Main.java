d6dd329e09f46cd03c372a2e043d00dc8c07d1b4
marco <marco@nhnedu.com>
2021-09-30 16:24:10 +0900

    commit 03

--- src/com/nhnacademy/Main.java
+++ src/com/nhnacademy/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("test1");
-        System.out.println("test2");
-        System.out.println("test3!");
-        System.out.println("test4!");
+        
     }
 }
