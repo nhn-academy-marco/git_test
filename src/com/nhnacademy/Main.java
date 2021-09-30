889076b5474a6d4390db9c39a117a92138952f07
marco <marco@nhnedu.com>
2021-09-30 16:09:07 +0900

    commit04

--- src/com/nhnacademy/Main.java
+++ src/com/nhnacademy/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
     }
 }
