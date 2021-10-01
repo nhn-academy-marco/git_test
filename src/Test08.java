d75c53fa283e13120b361059abb034ba7f7dc587
marco <marco@nhnedu.com>
2021-10-01 10:46:07 +0900

    test08- commit

---
+++ src/Test08.java
@@ -0,0 +1,13 @@
+/**
+ * <pre>
+ * Copyright 2021 NHN Entertainment Corp. All rights Reserved.
+ * NHN Entertainment PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * </pre>
+ * <p>
+ * Desc :
+ *
+ * @Author : marco@nhnedu.com
+ * @Date : 01/10/2021
+ */
+public class Test08 {
+}
